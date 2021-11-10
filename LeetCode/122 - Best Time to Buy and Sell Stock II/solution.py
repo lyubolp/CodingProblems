b11b33dcc7b459a40e3e175effe97672faa1ea1d
lyubolp <lyubo.karev@gmail.com>
2021-11-10 07:40:02 +0200

    Daily coding 10112021

---
+++ LeetCode/122 - Best Time to Buy and Sell Stock II/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if len(prices) < 2:
+            return 0
+        
+        profit = 0
+        
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i - 1]:
+                profit += prices[i] - prices[i - 1]
+        
+        return profit
+
