d27d9d6687f2aebab5536c931e067c8ea0d5f588
lyubolp <lyubo.karev@gmail.com>
2021-12-16 18:09:39 +0200

    Added day12 pt2

--- AdventOfCode2021/day12/12_02.py
+++ AdventOfCode2021/day12/12_02.py
@@ -2,8 +2,10 @@
 import sys
 from typing import List, Tuple, Dict, Set
 
+
 def read_input() -> List[str]:
     return [line.strip() for line in sys.stdin if line != '\n']
+
 
 def build_adj_list(lines: List[str]) -> Dict[str, List[str]]:
     result = {}
@@ -22,26 +24,39 @@ def build_adj_list(lines: List[str]) -> Dict[str, List[str]]:
 
     return result
 
-def dfs(adj_list: Dict[str, List[str]], current: str, visited: Dict[str, bool], path: List[str], paths: List[List[str]]):
+
+def dfs(adj_list: Dict[str, List[str]], current: str, visited: Dict[str, int], path: List[str], paths: List[List[str]], is_double_visit_done: bool):
     if current.islower():
-        visited[current] = True
+        if current in visited:
+            visited[current] += 1
+        else:
+            visited[current] = 1
+
     path.append(current)
 
     if current == 'end':
         paths.append(path.copy())
     else:
         for neighbour in adj_list[current]:
-            if neighbour not in visited or visited[neighbour] == False:
-                dfs(adj_list, neighbour, visited, path, paths)
-    
+            if neighbour == 'start':
+                continue
+
+            if neighbour not in visited or visited[neighbour] < 1:
+                dfs(adj_list, neighbour, visited, path, paths, is_double_visit_done)
+            elif visited[neighbour] < 2 and not is_double_visit_done:
+                dfs(adj_list, neighbour, visited, path, paths, True)
+
+
     path.pop()
-    visited[current] = False
+    if current.islower():
+        visited[current] -= 1
+
 
 if __name__ == "__main__":
     lines = read_input()
     adj_list = build_adj_list(lines)
 
     paths = []
-    dfs(adj_list, "start", {}, [], paths)
+    dfs(adj_list, "start", {}, [], paths, False)
 
     print(len(paths))
