080fd8b6d4688a13863f64360087fca6a360d76d
lyubolp <lyubo.karev@gmail.com>
2022-01-24 07:48:08 +0200

    Daily coding 24012022

---
+++ LeetCode/1313 - Decompress Run-Length Encoded List/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def decompressRLElist(self, nums: List[int]) -> List[int]:
+        result = []
+        for i in range(0, len(nums), 2):
+            freq, val = nums[i], nums[i+1]
+            result += [val] * freq
+        
+        return result
+
