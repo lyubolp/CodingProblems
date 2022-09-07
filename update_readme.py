bc7fe98ef62391d9c563560b2988117104811456
Lyuboslav Karev <lyubo.karev@gmail.com>
2022-09-08 00:05:36 +0300

    Added a cool script to automatically updated the README

---
+++ update_readme.py
@@ -0,0 +1,59 @@
+import os
+
+extensions_to_normal_name = {
+        'py': 'Python',
+        'rs': 'Rust',
+        'sh': 'Bash',
+        'cpp': 'C++',
+        'hs': 'Haskell'
+}
+
+def get_extension(filename: str) -> str:
+    return filename.split('.')[1]
+def count_extensions(extensions) -> dict:
+    result = {}
+    for extension in extensions:
+        if extension in result:
+            result[extension] += 1
+        else:
+            result[extension] = 1
+
+    return result
+
+def convert_to_markdown(counted_extensions: dict) -> str:
+    total = 0
+
+    result = '| Language: | Problems solved: |\n'
+    result += '| --------- | ---------------- |\n'
+
+    for language in counted_extensions:
+        count = counted_extensions[language]
+        result += f'| {language} | {count} |\n'
+        total += count
+    
+    result += f'| **Total:** | {total} |\n'
+
+    return result
+
+if __name__ == "__main__":
+    dirs_to_ignore = ['./.git']
+
+    all_files = [files for dirname, subdirs, files in os.walk('.') if not any(dir_pattern in dirname for dir_pattern in dirs_to_ignore)][1:]
+
+    all_files = sum(all_files, [])
+    all_extensions = [get_extension(filename) for filename in all_files]
+
+    all_code_extensions = [extensions_to_normal_name[extension] for extension in all_extensions if extension != 'txt']
+
+    counted_extensions = count_extensions(all_code_extensions)
+    markdown = convert_to_markdown(counted_extensions)
+
+    with open('README.md', 'r+') as fp:
+        line = fp.readline()
+
+        while line != '## Languages used:\n':
+            print(line)
+            line = fp.readline()
+        
+        fp.seek(fp.tell())
+        fp.write(markdown)
