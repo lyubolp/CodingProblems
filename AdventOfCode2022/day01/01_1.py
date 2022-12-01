94c9448c3898571a8117d1e9ac7d38891a1b7cab
lyubolp <lyubo.karev@gmail.com>
2022-12-01 08:26:48 +0200

    Advent of Code, day 1

---
+++ AdventOfCode2022/day01/01_1.py
@@ -0,0 +1,20 @@
+import sys
+
+def read_input() -> list[str]:
+    return [line for line in sys.stdin]
+
+if __name__ == "__main__":
+    inputs = [user_input.strip() for user_input in read_input()]
+
+    rations = []
+    prev = 0
+    for i, item in enumerate(inputs):
+        if item == '':
+            rations.append(inputs[prev:i])
+            prev = i + 1
+            
+    rations.append(inputs[prev:])
+
+    rations = [sum(int(ration) for ration in rations_per_elf) for rations_per_elf in rations]
+
+    print(max(rations))
