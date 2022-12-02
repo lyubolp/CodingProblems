a4f41cec2fddf812b003e34ab122035b295e1809
lyubolp <lyubo.karev@gmail.com>
2022-12-02 08:38:43 +0200

    readme script update

--- update_readme.py
+++ update_readme.py
@@ -9,6 +9,9 @@ extensions_to_normal_name = {
 }
 
 def get_extension(filename: str) -> str:
+    if filename.find('.') == -1:
+        return ''
+
     return filename.split('.')[1]
 def count_extensions(extensions) -> dict:
     result = {}
@@ -37,13 +40,13 @@ def convert_to_markdown(counted_extensions: dict) -> str:
 
 if __name__ == "__main__":
     dirs_to_ignore = ['./.git']
-
+    extensions_to_ignore = ['txt', '']
     all_files = [files for dirname, subdirs, files in os.walk('.') if not any(dir_pattern in dirname for dir_pattern in dirs_to_ignore)][1:]
 
     all_files = sum(all_files, [])
     all_extensions = [get_extension(filename) for filename in all_files]
 
-    all_code_extensions = [extensions_to_normal_name[extension] for extension in all_extensions if extension != 'txt']
+    all_code_extensions = [extensions_to_normal_name[extension] for extension in all_extensions if extension not in extensions_to_ignore]
 
     counted_extensions = count_extensions(all_code_extensions)
     markdown = convert_to_markdown(counted_extensions)
