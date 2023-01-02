a32f160c9a72c02096da5706c01ca4dcef6800bc
lyubolp <lyubo.karev@gmail.com>
2023-01-02 20:56:27 +0200

    update_readme now writes languages sorted by amount of files

--- update_readme.py
+++ update_readme.py
@@ -30,6 +30,7 @@ def convert_to_markdown(counted_extensions: dict) -> str:
     result = '| Language: | Problems solved: |\n'
     result += '| --------- | ---------------- |\n'
 
+    counted_extensions = dict(sorted(counted_extensions.items(), key=lambda item: item[1], reverse=True))
     for language in counted_extensions:
         count = counted_extensions[language]
         result += f'| {language} | {count} |\n'
@@ -40,16 +41,18 @@ def convert_to_markdown(counted_extensions: dict) -> str:
     return result
 
 if __name__ == "__main__":
-    dirs_to_ignore = ['./.git']
-    extensions_to_ignore = ['txt', '']
+    dirs_to_ignore = ['./.git', '.vscode', './.github']
+    extensions_to_ignore = ['txt', '', 'json', 'xml', 'gitignore', 'md']
     all_files = [files for dirname, subdirs, files in os.walk('.') if not any(dir_pattern in dirname for dir_pattern in dirs_to_ignore)][1:]
 
     all_files = sum(all_files, [])
     all_extensions = [get_extension(filename) for filename in all_files]
 
-    all_code_extensions = [extensions_to_normal_name[extension] for extension in all_extensions if extension not in extensions_to_ignore]
+    all_code_extensions = [extensions_to_normal_name[extension] for extension in all_extensions if extension not in extensions_to_ignore and extension in extensions_to_normal_name]
 
     counted_extensions = count_extensions(all_code_extensions)
+
+    print(counted_extensions)
     markdown = convert_to_markdown(counted_extensions)
 
     with open('README.md', 'r+') as fp:
