3c8d8ec2223292d4ac46e947993b376e145015f2
Lyuboslav Karev <lyubo.karev@gmail.com>
2024-12-14 21:25:06 +0200

    AOC day14

--- AdventOfCode2024/day14/14_1.py
+++ AdventOfCode2024/day14/14_1.py
@@ -1,5 +1,6 @@
 import sys
 from collections import namedtuple
+from functools import reduce
 
 Point = namedtuple("Point", ["x", "y"])
 
@@ -28,10 +29,40 @@ def draw_grid(bounds: Point, robots):
         print("")
 
 
+def move_robot(robot: Point, velocity: Point, seconds: int, bounds: Point) -> Point:
+    new_x = (robot.x + (seconds * velocity.x)) % bounds.x
+    new_y = (robot.y + (seconds * velocity.y)) % bounds.y
+
+    return Point(new_x, new_y)
+
+
+def count_robots_in_quadrant(robots: list[tuple[Point, Point]], bounds: Point, quadrant: int, quadrant_x: int, quadrant_y: int) -> int:
+    if quadrant == 1 or quadrant == 2:
+        start_x, end_x = 0, quadrant_x
+    else:
+        start_x, end_x = bounds.x - quadrant_x, bounds.x
+
+    if quadrant == 1 or quadrant == 3:
+        start_y, end_y = 0, quadrant_y
+    else:
+        start_y, end_y = bounds.y - quadrant_y, bounds.y
+
+    return sum(1 for robot, _ in robots if start_x <= robot.x < end_x and start_y <= robot.y < end_y)
+
+
 if __name__ == "__main__":
     lines = read_input()
 
     robots = [parse_line(line) for line in lines]
-
     bounds = Point(7, 11)
-    draw_grid(bounds, robots)
+    bounds = Point(103, 101)
+
+    seconds = 100
+    robots = [(move_robot(robot, velocity, seconds, bounds), velocity) for robot, velocity in robots]
+
+    quadrant_x = bounds.x // 2
+    quadrant_y = bounds.y // 2
+
+    robots_in_quadrants = [count_robots_in_quadrant(robots, bounds, quadrant, quadrant_x, quadrant_y) for quadrant in range(1, 5)]
+
+    print(reduce(lambda x, y: x * y, robots_in_quadrants, 1))
